04560b5c368ebdbfbfbb09c1f9e05e1173678ae9
Heber <heberyesiddazatoloza@gmail.com>
2025-11-01 01:04:15 -0500

    fix: Use python3 -m pip instead of pip command

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd backend
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+python3 manage.py migrate --noinput
+python3 manage.py collectstatic --noinput
 gunicorn config.wsgi:application --bind 0.0.0.0:$PORT --log-file -
