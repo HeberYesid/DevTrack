e247539d21777c5d3b204ae68518fc40d0253cf7
Heber <heberyesiddazatoloza@gmail.com>
2025-11-01 00:59:27 -0500

    fix: Railway pip command - simplified nixpacks with bash script

--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
-#!/usr/bin/env bash
-uvicorn main:app --host 0.0.0.0 --port $PORT
+#!/bin/bash
+cd backend
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+gunicorn config.wsgi:application --bind 0.0.0.0:$PORT --log-file -
